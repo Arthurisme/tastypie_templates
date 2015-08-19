eea6d523efe94590fe94c12765377db251cf01cd
Arthurisme <is@live.ca>
2015-08-19 17:16:44 -0400

    v1.06-json-working

--- talk_project/urls.py
+++ talk_project/urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls import patterns, include, url
 from talk_project.views import logout_page
+from talk.api import PostResource
+
+from tastypie.api import Api
+
+entry_resource = PostResource()
 
 from django.contrib import admin
 admin.autodiscover()
+
+
+
+v1_api = Api(api_name='v1')
+v1_api.register(PostResource())
 
 
 urlpatterns = patterns(
@@ -11,4 +21,13 @@ urlpatterns = patterns(
     url(r'^login/', 'django.contrib.auth.views.login'),
     (r'^logout/$', logout_page),
     url(r'^', include('talk.urls')),
+    (r'^api/', include(entry_resource.urls)),
+     (r'^api/', include(v1_api.urls)),
 )
+
+
+
+
+
+
+
